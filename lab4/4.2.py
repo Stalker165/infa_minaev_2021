f4ef50c74d10d2814969d91286a738a3bcb8db99
Minaev Artyom <minaev.artem@phystech.edu>
2021-09-27 12:04:39 +0300

    updated 4.1 added 4.2 (not working correctly)

---
+++ lab4/4.2.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.draw import *
+
+pygame.init()
+
+FPS = 30
+screen = pygame.display.set_mode((794, 1123))
+
+rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
+polygon(screen, (255, 255, 0), [(100,100), (200,50),
+                               (300,100), (100,100)])
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+pygame.quit()
