c120c9723dde8b506f498c647322bc32100aec5a
Minaev Artyom <minaev.artem@phystech.edu>
2021-10-03 18:03:07 +0300

    updated 4.2

--- lab4/4.2.py
+++ lab4/4.2.py
@@ -3,12 +3,67 @@ from pygame.draw import *
 
 pygame.init()
 
-FPS = 30
-screen = pygame.display.set_mode((794, 1123))
+def glare(color, x, y, r):
+    # имитация источника света
+    layer = pygame.Surface((screen_x, screen_y))
+    layer.set_colorkey((0, 0, 0))
+    for i in range(int(r)):  # здесь он ругался на то, что r это float, не знаю, почему
+        layer.set_alpha(25 - i*25/r)
+        ellipse(layer, color, (x - 2*i, y - 2*i, 4*i, 4*i))
+        screen.blit(layer, (0, 0))
 
-rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-polygon(screen, (255, 255, 0), [(100,100), (200,50),
-                               (300,100), (100,100)])
+def sun(color, x, y, a, b, w):
+    # солнышко
+    glare(color, x, y, w * 2.5)
+    glare(color, x - a + w/2, y, w * 2)
+    glare(color, x + a - w/2, y, w * 2)
+    glare(color, x, y - b + w/2, w * 2)
+    glare(color, x, y + b - w/2, w * 2)
+    # размеры бликов подобраны в соответствии с размерами солнца, где w - ширина кольца на картинке
+
+    layer = pygame.Surface((screen_x, screen_y))
+    layer.set_colorkey((0, 0, 0))
+    layer.set_alpha(150)
+    ellipse(layer, color, (x-a, y-b, 2*a, 2*b), w)
+    ellipse(layer, color, (x - 30, y - 30, 60, 60))
+    screen.blit(layer, (0, 0))
+
+    layer = pygame.Surface((screen_x, screen_y))
+    layer.set_colorkey((0, 0, 0))
+    layer.set_alpha(150)
+    rect(layer, color, (x-a, y-20, 2*a, 40))
+    screen.blit(layer, (0, 0))
+
+    layer = pygame.Surface((screen_x, screen_y))
+    layer.set_colorkey((0, 0, 0))
+    layer.set_alpha(150)
+    rect(layer, color, (x-20, y-b, 40, 2*b))
+    screen.blit(layer, (0, 0))
+
+#def bear(x, y, m):
+
+
+#def fish (x,y):
+
+
+
+FPS = 30
+screen_x = 794
+screen_y = 1123
+screen = pygame.display.set_mode((screen_x, screen_y))
+
+horizont = screen_y // 2  # линия горизонта
+
+rect(screen, (0, 255, 255), (0, 0, screen_x, horizont))  # вторая точка указывается относительно первой!
+rect(screen, (230, 230, 230), (0, horizont, screen_x, screen_y))
+line(screen, (0, 0, 0), (0, horizont), (screen_x, horizont))
+circle(screen, (255, 220, 0), (200, 200), 100, 0)
+polygon(screen, (0, 0, 0), [(120, 120), (130, 120),
+                            (185, 165), (180, 170)])
+
+sun((255, 246, 213), 472, 193, 230, 230, 40)
+
+pygame.image.save(screen, "bear.png")
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -19,5 +74,6 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
+
 
 pygame.quit()
--- main.py
+++ main.py
@@ -3,22 +3,67 @@ from pygame.draw import *
 
 pygame.init()
 
+def glare(color, x, y, r):
+    # имитация источника света
+    layer = pygame.Surface((screen_x, screen_y))
+    layer.set_colorkey((0, 0, 0))
+    for i in range(int(r)):  # здесь он ругался на то, что r это float, не знаю, почему
+        layer.set_alpha(25 - i*25/r)
+        ellipse(layer, color, (x - 2*i, y - 2*i, 4*i, 4*i))
+        screen.blit(layer, (0, 0))
+
+def sun(color, x, y, a, b, w):
+    # солнышко
+    glare(color, x, y, w * 2.5)
+    glare(color, x - a + w/2, y, w * 2)
+    glare(color, x + a - w/2, y, w * 2)
+    glare(color, x, y - b + w/2, w * 2)
+    glare(color, x, y + b - w/2, w * 2)
+    # размеры бликов подобраны в соответствии с размерами солнца, где w - ширина кольца на картинке
+
+    layer = pygame.Surface((screen_x, screen_y))
+    layer.set_colorkey((0, 0, 0))
+    layer.set_alpha(150)
+    ellipse(layer, color, (x-a, y-b, 2*a, 2*b), w)
+    ellipse(layer, color, (x - 30, y - 30, 60, 60))
+    screen.blit(layer, (0, 0))
+
+    layer = pygame.Surface((screen_x, screen_y))
+    layer.set_colorkey((0, 0, 0))
+    layer.set_alpha(150)
+    rect(layer, color, (x-a, y-20, 2*a, 40))
+    screen.blit(layer, (0, 0))
+
+    layer = pygame.Surface((screen_x, screen_y))
+    layer.set_colorkey((0, 0, 0))
+    layer.set_alpha(150)
+    rect(layer, color, (x-20, y-b, 40, 2*b))
+    screen.blit(layer, (0, 0))
+
+#def bear(x, y, m):
+
+
+#def fish (x,y):
+
+
+
 FPS = 30
-screen = pygame.display.set_mode((400, 400))
+screen_x = 794
+screen_y = 1123
+screen = pygame.display.set_mode((screen_x, screen_y))
 
-rect(screen, (255, 255, 255), (0, 0, 400, 400))
+horizont = screen_y // 2  # линия горизонта
+
+rect(screen, (0, 255, 255), (0, 0, screen_x, horizont))  # вторая точка указывается относительно первой!
+rect(screen, (230, 230, 230), (0, horizont, screen_x, screen_y))
+line(screen, (0, 0, 0), (0, horizont), (screen_x, horizont))
 circle(screen, (255, 220, 0), (200, 200), 100, 0)
-circle(screen, (255, 0, 0), (165, 170), 18, 0)
-circle(screen, (255, 0, 0), (235, 170), 15, 0)
-circle(screen, (0, 0, 0), (165, 170), 7, 0)
-circle(screen, (0, 0, 0), (235, 170), 6, 0)
-#line(screen, (0, 0, 0), (120, 120), (190, 170), 10)
-polygon(screen, (0, 0, 0), [(120, 120),(130, 120),(185, 165),(180, 170)])
-polygon(screen, (0, 0, 0), [(280, 120),(270, 120),(215, 165),(220, 170)])
+polygon(screen, (0, 0, 0), [(120, 120), (130, 120),
+                            (185, 165), (180, 170)])
 
-x1 = 150; y1 = 240
-x2 = 250; y2 = 260
-rect(screen, (0, 0, 0), (150, 240, 100, 20))
+sun((255, 246, 213), 472, 193, 230, 230, 40)
+
+pygame.image.save(screen, "bear.png")
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -29,5 +74,6 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
+
 
 pygame.quit()
